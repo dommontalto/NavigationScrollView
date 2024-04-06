ba1dab170e8d9296302016bb4f36bbee6ee22edd
dmontalto <dmontalto@imperosoftware.com>
2024-04-06 07:27:19 +0100

    Initial Commit

---
+++ NavigationScrollView/NavigationScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationScrollViewApp.swift
+//  NavigationScrollView
+//
+//  Created by Dominic Montalto on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
